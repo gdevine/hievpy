eed966dadd20dfaa29e1a4a7b632f1969d516dfc
Gerard Devine <g.devine@westernsydney.edu.au>
2018-05-04 10:51:08 +1000

    Added Simple TOA5 timeseries plotter

--- testhievpy.py
+++ testhievpy.py
@@ -1,13 +1,16 @@
 import os
 import hievpy
 
+
+# Set the base URL of the HIEv instance being called against
+base_url = 'https://hiev.westernsydney.edu.au/'
 # Set API/Auth token for all calls to the HIEvPy library
 api_token = os.environ['HIEV_API_KEY']
 
 
 # 1. Search API test
-#-------------------------------------------------------
-results = hievpy.search(api_token, experiments=['82'], upload_from_date="2018-05-01")
+# -------------------------------------------------------
+results = hievpy.search(api_token, base_url, experiments=['82'], upload_from_date="2018-05-01")
 
 
 # 2. Download file(s) locally
@@ -16,23 +19,19 @@ for result in results:
     hievpy.download(api_token, result)
 
 
-# 3. Load a TOA5 file into a pandas dataframe
-#-------------------------------------------------------
-df = hievpy.toa5_to_df(api_token, results[0])
+# 4. Print summary information about a TOA5 file
+# -------------------------------------------------------
+hievpy.toa5_summary(api_token, results[1])
 
 
+# 5. Load a TOA5 file into a pandas dataframe
+# -------------------------------------------------------
+df = hievpy.toa5_to_df(api_token, results[1])
 
-# df = hievpy.toa5_info(os.environ['HIEV_API_KEY'], myfile[0])
-#
-# df.reset_index()
-#
-# # x = df['TIMESTAMP']
-# y1 = df['RH_Avg']
-#
-# plt.style.use('presentation')
-# plt.plot(y1,use_index=True)
-#
-print('bla')
 
-# hp.reg_lin(x=df['VW_Avg(1)'], y=df['VW_Avg(2)'])
+# 6. Generate a simple plot of a single variable from a HIEv TOA5 file
+# -------------------------------------------------------
+hievpy.plot_toa5_var(api_token, results[1], 'VW_Avg(1)')
 
+
+print('complete')
