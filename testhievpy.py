b0032f73275c6081f6af6009e4606f3ab852006f
Gerard Devine <g.devine@westernsydney.edu.au>
2018-05-14 08:34:09 +1000

    Added 'Upload' function

--- testhievpy.py
+++ testhievpy.py
@@ -9,9 +9,8 @@ api_token = os.environ['HIEV_API_KEY']
 
 
 # xx = hievpy.search_download(api_token, experiments=['82'], upload_from_date="2018-05-01")
-df = hievpy.search_load_toa5df(api_token, filename="DRL_AUTO_450LO_SOIL_R", from_date="2017-01-02", to_date="2018-01-08")
+# df = hievpy.search_load_toa5df(api_token, filename="DRL_AUTO_450LO_SOIL_R", from_date="2017-01-02", to_date="2018-01-08")
 
-print('bla')
 #
 # # 1. Search API test
 # # -------------------------------------------------------
@@ -36,7 +35,27 @@ print('bla')
 #
 # # 6. Generate a simple plot of a single variable from a HIEv TOA5 file
 # # -------------------------------------------------------
-hievpy.plot_toa5df_var(api_token, df, 'VW_Avg(1)')
+# hievpy.plot_toa5df_var(df, 'VW_Avg(1)')
 #
 #
-# print('complete')
+
+# Set path of file to be uploaded
+upload_file = '/Users/gerarddevine/dev/python/hiev/hievpy/DanData.txt'
+
+# -------------------------------------------------------------------------
+# Set metadata variables for upload
+metadata = {"experiment_id":77,
+            "type":"RAW",
+            "description":"This is a dummy description for file upload testing",
+            "creator_email":"g.devine@westernsydney.edu.au",
+            "contributor_names":['Tom Smith, t.smith@google.com', 'Jane White, J.White@aol.com', 'Frank Blank, f.black@yahoo.com'],
+            "label_names":'"Rainfall","Environment","TOA5"',
+            "grant_numbers": '"ZXY7654","PRQ53422"',
+            "related_websites":'"http://www.bom.org.au","http://www.westernsydney.edu.au"',
+            "start_time":'2014-01-01 12:11:10',
+            "end_time":'2014-12-30 14:09:08'
+            }
+# -------------------------------------------------------------------------
+
+hievpy.upload(api_token, upload_file, metadata)
+print('complete')
